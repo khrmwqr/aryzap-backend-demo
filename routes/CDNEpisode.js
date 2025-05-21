78eb9148905d6b88bc4143d9bbea0a387b0c46c7
Khurram Waqar <khurrambaigw@gmail.com>
2025-05-22 00:36:26 +0500

    Update CDNEpisode.js

--- routes/CDNEpisode.js
+++ routes/CDNEpisode.js
@@ -8,5 +8,6 @@ router.get('/ep/:episodeId', CDNEpisodeController.getSpecificCDNEpisode);
 router.post('/', CDNEpisodeController.createCDNEpisode);
 router.put('/:episodeId', CDNEpisodeController.updateCDNEpisode);
 router.delete('/:episodeId', CDNEpisodeController.deleteCDNEpisode);
+router.get('/pg/:seriesId', CDNEpisodeController.getSpecificCDNEpisodesBySeriesIDWithPagination); //this was pagination api
 
 module.exports = router;
