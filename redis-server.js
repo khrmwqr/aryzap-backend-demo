936a021cafaa1905feb3c1c035f13b4dd10e7f14
khrmwqr <khurram@arytech.com>
2025-05-30 18:47:25 +0800

    Adding new pull

--- redis-server.js
+++ redis-server.js
@@ -4,7 +4,7 @@ const redis = require('redis')
 const redisClient = redis.createClient({
     //url: 'redis://default:es6EOuZvnb+8QSQSpuNSjzQx0hSsdxHsYzdNRkLf9qMA0v8F56kQf0kupgQca3AdOUIDvPzBxtDKYAyM@50.7.28.250:6379',
     //url: 'redis://default:g1qj3Tx3YIcdTtDrpVFwnZAOURqfpgmq@redis-17988.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com:17988',
-    url: "redis://default:Asar@9301807@145.239.87.54:6378",
+    url: "redis://:P4DEH3hA3ONdRr5@192.168.0.3:6379",
     legacyMode: false
 });
 redisClient.on('error', (err) => {
